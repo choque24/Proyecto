837e18f1b49b251e2cfd1ae9d42ea1ae816d913c
choque24 <134898184+choque24@users.noreply.github.com>
2024-04-09 21:16:08 +0200

    Add files via upload

--- var_const.h
+++ var_const.h
@@ -1,8 +1,7 @@
-#include "WString.h"
 #include "Arduino.h"
 const int led0 = 10, led1 = 11, led2 = 12, led3 = 13, led9 = 9; //abre, cierra, pare, sensor 1 y 2
 const int boton = 8;          //abra, cierra, pare
-//const int espera = 22; //tiempo de cerrado y apertura Porton 
+const int espera = 22; //tiempo de cerrado y apertura Porton 
 const int SensorFCA = 2;    // Final de Carrera Abierto, una vez que llega a x punto manda una señal al puerto de arduino, HIGH
 const int SensorFCC = 3;    // Final de Carrera Cerrado
 bool estado_sensor = HIGH;
@@ -12,25 +11,4 @@ bool ultimo_sensor2 = HIGH;
 bool estado_boton = HIGH;
 bool ultimo_boton = HIGH;
 bool salida;         //LOW = porton cerrado
-unsigned long tiempo_actual;
-unsigned long anterior_tiempo;
-int boton_cerrado;
-int boton_abierto;
-enum estados{
-  abriendo = 1,
-  cerrando,
-  detenido
-};
-int  opciones[] = {
-  boton_cerrado = 1,
-  boton_abierto = 2
- };
-void apertura(){
-  digitalWrite(led1, HIGH);
-}
-void cerradura(){ // 
-  digitalWrite(led2, HIGH);
-} 
-void frenado(){ // FRENA
-  digitalWrite(led3, HIGH);
-}
+unsigned long tiempo_actual;            
